31ee2e6367dcd94f0a0111c06c2468e3cc05d271
Richard Claydon <richard@ugrworks.com>
2016-08-31 18:03:37 -0400

    Adding the AzureDocumentService stub.

---
+++ src/AspNet-AzureUpload.Service/Infrastructure/AzureDocumentService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace AspNet_AzureUpload.Service.Infrastructure
+{
+    public class AzureDocumentService : IDocumentService
+    {
+        public Task<string> UploadDocument(IFormFile document)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/AspNet-AzureUpload.Service/Infrastructure/IDocumentService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace AspNet_AzureUpload.Service
+{
+    public interface IDocumentService
+    {
+        Task<string> UploadDocument(IFormFile document);
+    }
+}
